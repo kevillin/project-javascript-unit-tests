2075655988de83d96b348f6695072b7f42baeaf8
Kevillin Santos <kevillin_santos@hotmail.com>
2022-06-20 17:20:00 -0300

    Requisito 2

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -23,9 +23,9 @@ const circle = require('../src/circle');
 
 describe('4 - Implemente os casos de teste para a função `circle`', () => {
   it('Verifica se ao receber um raio, a função `circle` retorna um objeto contendo os valores esperados', () => {
-    fail('Teste vazio!');
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle retorna undefined, caso o parâmetro passado não seja um número.
+    expect(circle('n')).toBeUndefined();
     // Teste se circle retorna um objeto.
     // Teste se o objeto retornado possui 3 propriedades.
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
